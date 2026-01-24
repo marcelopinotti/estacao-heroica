bed1e96393f3ceaf61dd55948b5a084c965f5202
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-01-24 19:39:38 -0300

    chore: inclui dto and mapper nas classes

--- src/main/java/marcelo/HeroGarage/Carros/CarrosDTO.java
+++ src/main/java/marcelo/HeroGarage/Carros/CarrosDTO.java
@@ -1,4 +1,22 @@
 package marcelo.HeroGarage.Carros;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import marcelo.HeroGarage.Personagem.PersonagemModel;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarrosDTO {
+
+
+    private Long id;
+    private String nome;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private PersonagemModel personagem;
+    private String cambio;
+    private String cor;
 }
--- src/main/java/marcelo/HeroGarage/Personagem/PersonagemDTO.java
+++ src/main/java/marcelo/HeroGarage/Personagem/PersonagemDTO.java
@@ -1,4 +1,21 @@
 package marcelo.HeroGarage.Personagem;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import marcelo.HeroGarage.Carros.CarrosModel;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonagemDTO {
+
+    private Long id;
+    private String nome;
+    private String desenho;
+    private int idade;
+    private String genero;
+    private List<CarrosModel> carros;
 }
